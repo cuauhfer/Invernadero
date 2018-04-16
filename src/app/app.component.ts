95ddfd50644d4e58364754bf31a2c333a26567e8
My Name <you@example.com>
2018-04-16 14:03:07 -0500

    Agrega pantalla de cultivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { LoginPage} from "../pages/login/login";
+import { CultivosPage} from "../pages/cultivos/cultivos";
 @Component({
   templateUrl: 'app.html'
 })
